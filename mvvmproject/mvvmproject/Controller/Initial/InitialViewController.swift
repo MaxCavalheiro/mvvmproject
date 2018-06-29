646361db86679460a544b34dbcd5ff9a212411d5
Max Cavalheiro <max.cavalheiro@nodo.cc>
2018-06-29 16:47:06 -0300

    Added carthage into project

---
+++ mvvmproject/mvvmproject/Controller/Initial/InitialViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  mvvmproject
+//
+//  Created by Gustavo Ludtke on 6/29/18.
+//  Copyright © 2018 Nodo Digital. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
